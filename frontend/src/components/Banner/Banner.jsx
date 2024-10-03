44e5af52402590089632c7344e9e4537c2c95bd3
Shmalii <Shmaliy2@gmai.com>
2024-10-03 22:27:16 +0200

    endBannerTask#6

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import s from "./Banner.module.css";
+import image from "../../assets/images/Banner_img.png";
 
 export default function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <section className={s.container}>
+      <img src={image} alt="GreenShop" className={s.image} />
+
+      <div className={s.containerText}>
+        <h1 className={s.containerTitle}>Amazing Discounts on Garden Products!</h1>
+        <button className={s.containerButton}>Check out</button>
+      </div>
+    </section>
+  );
 }
