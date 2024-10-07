66346459976e8cf1cc40e9d10b73bb65187cd45b
Shmalii <Shmaliy2@gmai.com>
2024-10-07 18:55:16 +0200

    ErrorPage

--- frontend/src/Pages/Error404Page/Error404Page.jsx
+++ frontend/src/Pages/Error404Page/Error404Page.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import cactus from "../../assets/images/Error404Page_cactus.svg";
+import React from "react";
+import s from "./Error404Page.module.css";
+import fourImage from "../../assets/images/Error404Page_4.svg";
 
 export default function Error404Page() {
   return (
-    <div>Error404Page</div>
-  )
+    <div className={s.errorContainer}>
+      <div>
+      <img src={fourImage} alt="Number four" className={s.fourImage} />
+      <img src={cactus} alt="Cactus" className={s.errorImage} />
+      <img src={fourImage} alt="Number four" className={s.fourImage} />
+      </div>
+      <p className={s.errorText}>Page Not Found</p>
+      <p className={s.errorDescription}>
+        We're sorry, the page you requested could not be found.
+        <br />
+        Please go back to the homepage.
+      </p>
+      <a href="/" className={s.goHomeBtn}>
+        Go Home
+      </a>
+    </div>
+  );
 }
