8c07b8dfec599d9d534b718a8f74385062315894
Shmalii <Shmaliy2@gmai.com>
2024-10-15 08:05:35 +0200

    doneTask#22

--- frontend/src/components/CategoriesItem/CategoriesItem.jsx
+++ frontend/src/components/CategoriesItem/CategoriesItem.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import s from "./CategoriesItem.module.css";
+import { Link } from "react-router-dom";
 
-export default function CategoryItem({ id, image, title }) {
+export default function CategoryItem({ id, image, title}) {
   return (
+    <Link to={`/category/${id}`}>
     <div className={s.categoryCard}>
       <img
         src={`http://localhost:3333${image}`}
@@ -11,5 +13,6 @@ export default function CategoryItem({ id, image, title }) {
       />
       <h3 className={s.categoryTitle}>{title}</h3>
     </div>
+    </Link>
   );
 }
