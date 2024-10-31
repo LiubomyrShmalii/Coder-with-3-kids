e414d34c1aa61ba90f647e5ef976168b410dc169
Shmalii <Shmaliy2@gmai.com>
2024-11-01 00:50:33 +0100

    endTask#37

--- frontend/src/components/CategoriesContainer/CategoriesContainer.jsx
+++ frontend/src/components/CategoriesContainer/CategoriesContainer.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import s from "./CategoriesContainer.module.css";
 import { getAllCategories } from "../../requests/allCategories.js";
 import CategoryItem from "../CategoryItem/CategoryItem.jsx";
+import Skeleton from "../Skeleton/Skeleton"; // Імпортуємо скелетон
 
 export default function CategoriesContainer() {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true); // Додаємо стан для завантаження
 
   useEffect(() => {
-    dispatch(getAllCategories);
-  }, []);
+    dispatch(getAllCategories())
+      .then(() => setIsLoading(false))
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+        setIsLoading(false);
+      });
+  }, [dispatch]);
 
   const partCategories = useSelector((store) => store.categories).slice(0, 4);
 
@@ -25,9 +32,11 @@ export default function CategoriesContainer() {
       </div>
 
       <div className={s.categories}>
-        {partCategories.map((el) => (
-          <CategoryItem key={el.id} {...el} />
-        ))}
+        {isLoading ? (
+          <Skeleton count={4} /> // Використовуємо скелетон для завантаження
+        ) : (
+          partCategories.map((el) => <CategoryItem key={el.id} {...el} />)
+        )}
       </div>
     </section>
   );
